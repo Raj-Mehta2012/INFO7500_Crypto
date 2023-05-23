6361f712a671f0b729bf839909a3d0cad0db52e5
Raj Mehta <>
2023-05-23 17:22:21 -0400

    Updated the coverage in hardhat.config.js

--- v1.0/hardhat.config.js
+++ v1.0/hardhat.config.js
@@ -4,4 +4,13 @@ require('solidity-coverage')
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  coverage: {
+    enabled: true,
+    reporter: ["html", "lcov"],
+    outputDir: "./coverage",
+  },
+  networks: {
+    hardhat: {
+    }
+  }
 };
