94a6b7c73d3009c2aafe3523ae4ec84dce8672e0
Raj Mehta <>
2023-06-04 19:56:27 -0400

    Added Test Cases

--- v2.0/contracts/NFT.sol
+++ v2.0/contracts/NFT.sol
@@ -1,4 +1,6 @@
-//SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
+// Author: Raj Mehta
+// Version : 2.0
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
